7e8cd94919306373e9bf487c79853170d2041249
Pierfrancesco Ardino <pierfrancesco.ardino@studenti.unitn.it>
2017-09-26 22:29:55 +0200

    Gui Improved, Resizing and Keyboard implementation

--- calculator.cpp
+++ calculator.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 using namespace Gtk;
-class Calculator : public Window
-{
+class Calculator : public Window {
     // controls
 
 protected:
@@ -21,12 +20,24 @@ protected:
     vector<Button> operationButton;
     bool expResult;
 
-    // methods
 
-public: Calculator()
-    {
+    // methods
+private:
+    bool on_key_press_event(GdkEventKey *event) override;
+    void numberButtonClicked(int index);
+    void operationButtonClicked(int index);
+    void parButtonClicked(string parenthesis);
+    void equalButtonClicked();
+
+
+
+public:
+    Calculator();
+};
+Calculator::Calculator(){
         expResult=false;
         buttonMatrix.resize(3);
+        add_events(Gdk::KEY_PRESS_MASK);
         set_border_width(12);
         for(int i=0; i< buttonMatrix.size();i++)
         {
@@ -45,19 +56,6 @@ public: Calculator()
         entryTextToDisplay.set_text("");
         calcGrid.attach(entryTextToDisplay,0,3,4,3);
 
-        /*// buttonDisplay
-        buttonDisplay.set_label("Display");
-        buttonDisplay.signal_clicked().connect
-                (
-                        sigc::mem_fun
-                                (
-                                        *this,
-                                        &Calculator::buttonDisplayClicked
-                                )
-                );
-        vBoxMain.add(buttonDisplay);
-        buttonDisplay.show();*/
-        //button0
         int i=2;
         int j=0;
         int label=1;
@@ -127,14 +125,22 @@ public: Calculator()
         rightParButton.set_label(")");
         rightParButton.signal_clicked().connect(sigc::bind<Glib::ustring>(sigc::mem_fun(*this, &Calculator::parButtonClicked),")"));
         calcGrid.attach(rightParButton,4,7,1,1);
+        for(Widget *obj : calcGrid.get_children())
+        {
+            obj->set_hexpand(true);
+            obj->set_vexpand(true);
+        }
+        calcGrid.set_row_spacing(10);
+        calcGrid.set_column_spacing(10);
+        calcGrid.set_margin_left(30);
+        calcGrid.set_margin_bottom(30);
+        calcGrid.set_margin_right(30);
+        calcGrid.set_margin_top(30);
         calcGrid.show_all();
-
-
     }
-
     // event handlers
 
-protected:void numberButtonClicked(int index)
+void Calculator::numberButtonClicked(int index)
     {
         if(expResult == true){
             entryTextToDisplay.set_text("");
@@ -150,8 +156,9 @@ protected:void numberButtonClicked(int index)
         text.append(to_string(index));
         }
         entryTextToDisplay.set_text(text);
+        entryTextToDisplay.set_position(text.size());
     }
-protected:void parButtonClicked(string parenthesis)
+void Calculator::parButtonClicked(string parenthesis)
     {
         if(expResult == true){
             entryTextToDisplay.set_text("");
@@ -161,7 +168,7 @@ protected:void parButtonClicked(string parenthesis)
         text.append(parenthesis);
         entryTextToDisplay.set_text(text);
     }
-protected:void operationButtonClicked(int index)
+void Calculator::operationButtonClicked(int index)
     {
         string label="";
         switch(index){
@@ -180,12 +187,12 @@ protected:void operationButtonClicked(int index)
             case 4:
                 label ="^";
                 break;
-        }
+                       }
         string text = entryTextToDisplay.get_text();
         text.append(label);
         entryTextToDisplay.set_text(text);
     }
-protected:void equalButtonClicked()
+void Calculator::equalButtonClicked()
     {
         std::string expression = entryTextToDisplay.get_text();
         entryTextToDisplay.set_text("");
@@ -198,17 +205,124 @@ protected:void equalButtonClicked()
         float result = evaluateExpression(postfixExpression);
         std::cout << "The result is :   \n" << result <<std::endl;
         entryTextToDisplay.set_text(to_string(result));
+        entryTextToDisplay.set_position(to_string(result).size());
         expResult = true;
 
     }
-};
+bool Calculator::on_key_press_event(GdkEventKey* key_event)
+    {
+        string command;
+        if((key_event->keyval == GDK_KEY_parenleft) &&
+           (key_event->state &(GDK_SHIFT_MASK | GDK_CONTROL_MASK | GDK_MOD1_MASK)) == GDK_SHIFT_MASK)
+        {
+            command= "(";
+        }else if((key_event->keyval == GDK_KEY_parenright) &&
+                 (key_event->state &(GDK_SHIFT_MASK | GDK_CONTROL_MASK | GDK_MOD1_MASK)) == GDK_SHIFT_MASK)
+        {
+            command= ")";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_0 || key_event->keyval == GDK_KEY_0)
+        {
+            command= "0";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_1 || key_event->keyval == GDK_KEY_1)
+        {
+            command= "1";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_2 || key_event->keyval == GDK_KEY_2)
+        {
+            command= "2";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_3 || key_event->keyval == GDK_KEY_3)
+        {
+            command= "3";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_4 || key_event->keyval == GDK_KEY_4)
+        {
+            command= "4";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_5 || key_event->keyval == GDK_KEY_5)
+        {
+            command= "5";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_6 || key_event->keyval == GDK_KEY_6)
+        {
+            command= "6";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_7 || key_event->keyval == GDK_KEY_7)
+        {
+            command= "7";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_8 || key_event->keyval == GDK_KEY_8)
+        {
+            command= "8";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_9 || key_event->keyval == GDK_KEY_9)
+        {
+            command= "9";
+        }
+        else if(key_event->keyval == GDK_KEY_plus || key_event->keyval == GDK_KEY_KP_Add){
+            command="+";
+        }
+        else if(key_event->keyval == GDK_KEY_minus || key_event->keyval == GDK_KEY_KP_Subtract){
+            command="-";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_Multiply){
+            command="*";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_Divide){
+            command="/";
+        } else if((key_event->keyval == GDK_KEY_asciicircum) &&
+                  (key_event->state &(GDK_SHIFT_MASK | GDK_CONTROL_MASK | GDK_MOD1_MASK)) == GDK_SHIFT_MASK)
+        {
+            command= "^";
+        }
+        else if(key_event->keyval == GDK_KEY_KP_Enter || key_event->keyval == GDK_KEY_Return)
+        {
+            equalButton.activate();
+        }
+        else if(key_event->keyval == GDK_KEY_BackSpace)
+        {
+            if(expResult !=true){
+                string tmp = entryTextToDisplay.get_text();
+                if (tmp != "")
+                {
+                    tmp.resize(tmp.size()-1);
+                    entryTextToDisplay.set_text(tmp);
+                    entryTextToDisplay.set_position(tmp.size());
+                }
+            }
+            else
+            {
+                entryTextToDisplay.set_text("");
+                entryTextToDisplay.set_position(0);
+                expResult=false;
+            }
+        }
+        if(command!="")
+        {
+            if(expResult == true){
+                entryTextToDisplay.set_text("");
+                expResult=false;
+            }
+            string text = entryTextToDisplay.get_text();
+            text.append(command);
+            entryTextToDisplay.set_text(text);
+            entryTextToDisplay.set_position(text.size());
+
+        }
+        std::cout<<"Key pressed " << key_event->keyval << std::endl;
+        return true;
+    }
 
 int main (int argc, char *argv[])
 {
     Glib::RefPtr<Gtk::Application> app =
             Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
     Calculator application;
-    application.set_default_size(300,200);
+    application.set_default_size(600,400);
+    application.set_hexpand(true);
+    application.set_vexpand(true);
     return app->run(application);
 }
 
